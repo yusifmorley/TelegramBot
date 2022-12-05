5749e56c44a68eccba6af52649235e08c3e9029b
yusifmorley <yusifmorley@gmail.com>
2022-12-05 22:59:21 +0800

    ok

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ import mysqlop
 import getConfig
 import getRadomTheme
 import time
-#主文件
+import threading
+
 
 myapi = getConfig.getTelegramId() # 机器人api
 updater = Updater(token=myapi, use_context=True)
@@ -20,8 +21,16 @@ dispatcher = updater.dispatcher
 banword = getbanword()
 logging.basicConfig(filename="Log/mylog", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)  # 日志
+logger=logging.getLogger(__name__)
 mydb = getConfig.getMysqlConfig()
 messids={}  #存储所有用户和bot 的massage_id  不包括抽取的图片 合成的主题 和随机返回的主题
+timeinter=1000 #定时
+tempcontext=None
+def play():
+    if len(messids)>0:
+
+    threading.Timer(timeinter, play).start()
+
 
 #合并主题和背景
 def combinThemeAndPic(update, context):
@@ -41,9 +50,7 @@ def downloadtheme(update, context):
     if flag is not None:  # 如果数据库里有 请求记录 则下载主题 否则pass
         file = context.bot.getFile(update.message.document.file_id)
         file.download("temptheme/" + update.message.document.file_name)
-        print("____________")
         recordSend(update, context, "我已收到主题文件啦！")
-        print("+++++++++++++")
         themeFileDowmloaded = "temptheme/" + update.message.document.file_name
         if flag == 1:
             mysqlop.updathemeadata(mydb, themeFileDowmloaded, update.effective_chat.id)
@@ -74,13 +81,10 @@ def handlePhoto(update, context):
             tim = time.localtime()
             timstr = time.strftime("%Y-%m-%d-%H-%M-%S", tim)
             recordSend(update, context, "这确实是一张图片！")
-            print(100000000)
-            print(update)
             file = context.bot.getFile(update.message.photo[2]['file_id'])
             photoPath = "tempthemephoto/" + timstr + ".jpg"
             file.download(photoPath)
             recordSend(update, context, "图片已下载")
-
             mysqlop.updatpicdata(mydb, photoPath, update.effective_chat.id)
             themepath = mysqlop.getthemepath(mydb, update.effective_chat.id)[0]
             if themepath is not None:
@@ -91,55 +95,29 @@ def handlePhoto(update, context):
                 mysqlop.deletelog(mydb, update.effective_chat.id)  # 删除 记录
             else:
                 recordSend(update, context, "请发送主题！")
-        else:
-            pass
+
+def onjoin(update,context):
+    print(update.effective_chat)
+    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
+
 
 def adminhanderex(update, context):  # 管理员
     text = update.effective_message.text
     os = deletetxt(banword,text)
-
     if os: #若存在违禁词
-
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
-
-        num= mysqlop.getbannum(mydb, update.effective_user.id)
-
-        if num==0:
-            mysqlop.creatbanuser(mydb,update.effective_user.id,os)
-
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text="您触发了违禁词:{}".format(os))
-
-            mysqlop.upbanuser(mydb,update.effective_user.id,os)
-
-        elif num<=2:
-            mysqlop.upbanuser(mydb,update.effective_user.id,os)
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text="您触发了违禁词:{}".format(os))
-        else:
-            try:
-                context.bot.restrict_chat_member(chat_id=update.effective_chat.id,
+        context.bot.restrict_chat_member(chat_id=update.effective_chat.id,
                                          user_id=update.effective_user.id,
                                          permissions=ChatPermissions(can_send_messages=False,
-                                                                 can_send_media_messages=False))
-
-                textlog = "用户id :" + str(
-                    update.effective_user.id) + " 用户名 :" + update.effective_user.full_name \
-                          + "已被永久封禁，由于多次触发违禁词：" + \
-                          mysqlop.getbanwords(mydb, update.effective_user.id) + "-" + os
-                context.bot.send_message(chat_id=update.effective_chat.id, text=textlog)
-                mysqlop.deletelog(mydb, update.effective_user.id)
-                logging.info(textlog)
-            except:
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text="权限不够 无法执行呦～")
-# def deletaBotMessage(update, context):
-#      print(update.message.from_user.id)
-#      mess=update.message.reply_text("Please delete")
-#      time.sleep(2)
-#      context.bot.delete_message(message_id=mess.message_id,chat_id=update.effective_chat.id)
+                                                                     can_send_media_messages=False))
+        logger.info(os)
 
 def recordSend(update, context, txt):
+    global tempcontext
+
+    tempcontext=context
+
+    newmid=update.effective_message.message_id
     id=update.message.from_user.id
     mes= context.bot.send_message(chat_id=update.effective_chat.id,
                                          text=txt)
@@ -148,6 +126,7 @@ def recordSend(update, context, txt):
     else:
         messids[id]=[]
         messids[id].append(mes)
+    messids[id].append(newmid)
 
 
 def delete(update,context):
@@ -161,6 +140,7 @@ def getRanTheme(update, context):
     recordSend(update, context, "这是您的主题文件，亲～")
 
 if __name__=="__main__":
+        threading.Timer(timeinter, play).start()
         mysqlop.initdb(mydb) #初始化 数据库
         try:
             combinss = CommandHandler('combinthemeandphoto', combinThemeAndPic)
@@ -183,6 +163,7 @@ if __name__=="__main__":
 
             adminhander = MessageHandler(Filters.text, adminhanderex)
             dispatcher.add_handler(adminhander)
+            dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members,onjoin))
 
             updater.start_polling()
         except mysql.connector.errors.OperationalError: #连接断开 重新链接
