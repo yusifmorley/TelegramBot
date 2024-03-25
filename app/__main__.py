dd6227dd3e9f6f60b59d557b397b898a68e0eccd
YusifMorley <yusifmorley@gmail.com>
2024-03-25 12:15:01 +0800

    telegram.ext包升级为21.0.1版本

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,12 @@
 import base64
 import os
 import traceback
-
 from PIL import Image
 import sqlalchemy.exc
 import telegram
-from telegram import Update, Bot, File, InlineKeyboardMarkup, Message, Chat
-from telegram.ext import Updater, CallbackContext, CallbackQueryHandler
-from telegram.ext import MessageHandler, Filters
+from telegram import Update, Bot, File, Message, Chat
+from telegram.ext import Updater, CallbackContext, CallbackQueryHandler, ContextTypes, Application
+from telegram.ext import MessageHandler, filters
 from telegram.ext import CommandHandler
 import mysql.connector
 from urllib3.exceptions import NewConnectionError
@@ -35,24 +34,21 @@ from app.util.db_op import clear
 from app.util.sync_public_attheme import sunc_ap
 from app.util.sync_public_desk import sync_dp
 
-my_github: str = ("欢迎使用主题生成机器人\n")
-
+my_github: str = "欢迎使用主题生成机器人\n"
 my_id = get_myid()
 myapi = get_config.get_telegram_id()  # 机器人api
 # public_IO:IO|None=None #供用户发送 document 形式的　图片用　
 session: Session = init_session()
 
-request_kwargs = {}
+proxy_url = None
 if os.environ.get('ENV') == 'dev':
-    request_kwargs = {
-        'proxy_url': 'http://127.0.0.1:10810/'}
+    proxy_url = "http://127.0.0.1:10810/"
     myapi = '6520279001:AAFlM8bPclv-dZvSERAbLihBNlMNVz2KRK0'  # 测试机器人id
 
-updater = Updater(token=myapi, use_context=True, request_kwargs=request_kwargs)
-commands =get_command()
-bot: Bot = updater.bot
+application = Application.builder().token(myapi).proxy_url(proxy_url).build()
+commands = get_command()
+bot: Bot = application.bot
 bot.set_my_commands(commands)
-dispatcher = updater.dispatcher
 
 # 日志
 logger = t_log.get_logger()
@@ -71,19 +67,21 @@ ban_words = admin_function.get_ban_word(BanWordObject)
 # 监控10个人
 mon_per = MonitorPerson(10)
 
-strinfo = get_command_str()
+str_info = get_command_str()
+
+
 # 合并主题和背景
-def on_join(update: Update, context: CallbackContext):
+async def on_join(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # 如果没有权限
     if app.admin.admin_function.bot_delete_permission(update, context) == 0:
         return
 
-    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
+    await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
 
 
-def admin_handle(update: Update, context: CallbackContext):  # 管理员
+async def admin_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):  # 管理员
 
-    if update.effective_message.chat.id !=-1001313322278:
+    if update.effective_message.chat.id != -1001313322278:
         return
 
     text = update.effective_message.text
@@ -92,12 +90,12 @@ def admin_handle(update: Update, context: CallbackContext):  # 管理员
         chat_id = update.message.chat_id
         # 下载主题文件
         bot = context.bot
-        theme_file = bot.get_file(theme_link)
+        theme_file = await bot.get_file(theme_link)
         # 发送主题文件给用户
-        bot.send_document(chat_id=chat_id, document=theme_file.file_id)
+        await bot.send_document(chat_id=chat_id, document=theme_file.file_id)
         return
     user = update.effective_message.from_user
-    
+
     if update.effective_message.chat.type == Chat.GROUP or update.effective_message.chat.type == Chat.SUPERGROUP:
         if hasattr(user, "id"):
             mon_per.run(user.id, user.first_name + " " + user.first_name, text, update, context, ban_words, logger)
@@ -105,36 +103,35 @@ def admin_handle(update: Update, context: CallbackContext):  # 管理员
         return
 
 
-
 @listen
-def get_ran_theme(update: Update, context: CallbackContext):
+async def get_ran_theme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     path = get_radom_link.get_random_theme()
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=path.rstrip("\n"))
-    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=path.rstrip("\n"))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
 
 
-def write_ban_word(update: Update, context: CallbackContext):
+async def write_ban_word(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.from_user.id != 507467074:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="您不是管理员！\n" + strinfo)
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="您不是管理员！\n" + str_info)
         return
     global ban_words
     admin_function.write_ban_word(BanWordObject, context.args[0])
     ban_words = admin_function.get_ban_word(BanWordObject)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="成功添加新的违禁词：" + context.args[0])
-    context.bot.delete_message(message_id=update.effective_message.message_id, chat_id=update.effective_chat.id)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="成功添加新的违禁词：" + context.args[0])
+    await context.bot.delete_message(message_id=update.effective_message.message_id, chat_id=update.effective_chat.id)
 
 
-def combin_theme(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="您不是管理员！\n" + strinfo)
+async def combin_theme(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="您不是管理员！\n" + str_info)
 
 
 # 错误处理
-def error_handler(update: Update, context: CallbackContext):
-    global  mydb
+async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global mydb
     try:
         raise context.error
 
@@ -156,45 +153,46 @@ def error_handler(update: Update, context: CallbackContext):
         session.close()
         app.model.models.reflush()
         logger.warning("已经重置 session ")
-            # 异常发生时的清理操作
+        # 异常发生时的清理操作
         info = traceback.format_exc()
-        context.bot.send_message(chat_id=my_id, text=f"出错了 {context.error},\n{update} \n{info}")
+        await context.bot.send_message(chat_id=my_id, text=f"出错了 {context.error},\n{update} \n{info}")
+
 
 @listen
-def get_android_theme(update: Update, context: CallbackContext):
+async def get_android_theme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     path = get_android.get_android_theme()
     fd = open("src/Theme/android-theme/" + path, "rb")
     data = fd.read()
     fd.close()
     preview_bytes = get_android.get_android_preview(path, data)
-    context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
     if preview_bytes:
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
 
 
 @listen
-def get_desktop_theme(update: Update, context: CallbackContext):
+async def get_desktop_theme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     path = get_desktop.get_desktop_theme()
     fd = open("src/Theme/desktop-theme/" + path, "rb")
     data = fd.read()
     fd.close()
     preview_bytes = get_desktop.get_desktop_preview(path, data)
-    context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
     if preview_bytes:
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
 
 
 @listen
-def get_ios_theme(update: Update, context: CallbackContext):
+async def get_ios_theme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     path = get_ios.get_random_theme()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=path.rstrip("\n"))
-    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=path.rstrip("\n"))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
 
 
 @listen
-def create_attheme(update: Update, context: CallbackContext):
+async def create_attheme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     same_primary_key = update.effective_user.id
     existing_user: CreateThemeLogo | None = session.get(CreateThemeLogo, same_primary_key)
     if existing_user:
@@ -206,11 +204,11 @@ def create_attheme(update: Update, context: CallbackContext):
         session.add(new_user)
 
     session.commit()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="请发送您的图片")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="请发送您的图片")
 
 
 @listen
-def create_tdesktop(update: Update, context: CallbackContext):
+async def create_tdesktop(update: Update, context: ContextTypes.DEFAULT_TYPE):
     same_primary_key = update.effective_user.id
     existing_user: CreateThemeLogo | None = session.get(CreateThemeLogo, same_primary_key)
     if existing_user:
@@ -221,11 +219,11 @@ def create_tdesktop(update: Update, context: CallbackContext):
         session.add(new_user)
 
     session.commit()
-    context.bot.send_message(chat_id=update.effective_chat.id, text="请发送您的图片")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="请发送您的图片")
 
 
 # 用户发送图片
-def base_photo(update: Update, context: CallbackContext, doucment_pt: str | None = None):
+async def base_photo(update: Update, context: CallbackContext, doucment_pt: str | None = None):
     # 只有图片
     if update.effective_message.chat.type == Chat.CHANNEL:
         return
@@ -256,22 +254,19 @@ def base_photo(update: Update, context: CallbackContext, doucment_pt: str | None
             existing_user.pic_path = doucment_pt
     else:
         pid = update.effective_message.photo[-1].file_id  # 最后一个是完整图片
-        pic_file = bot.get_file(pid)
+        pic_file = await bot.get_file(pid)
         user_id = update.effective_user.id
         # io重用
         bio = BytesIO()
         # 写入图片
         pic_p = "src/Photo/" + str(user_id) + ".png"
         fp = open(pic_p, "wb")
-
-        pic_file.download(out=bio)
+        await pic_file.download_to_memory(bio)
         fp.write(bio.getvalue())
         fp.close()
         # 更新数据库
-
         pic_bytes = bio.getvalue()
         bio.close()
-
         existing_user.pic_path = pic_p
 
     content: list = get_attheme_color_pic(pic_bytes)
@@ -280,12 +275,12 @@ def base_photo(update: Update, context: CallbackContext, doucment_pt: str | None
     if existing_user.flag != 4:
 
         reply_markup = get_kyb(content[0])
-        call_message: Message = update.message.reply_photo(content[1], caption="首先，请选择主题的背景颜色",
-                                                           reply_markup=reply_markup)
+        call_message: Message = await update.message.reply_photo(content[1], caption="首先，请选择主题的背景颜色",
+                                                                 reply_markup=reply_markup)
     else:
         reply_markup = get_desktop_kyb(content[0])
-        call_message: Message = update.message.reply_photo(content[1], caption="首先，请选择主题的背景颜色",
-                                                           reply_markup=reply_markup)
+        call_message: Message = await update.message.reply_photo(content[1], caption="首先，请选择主题的背景颜色",
+                                                                 reply_markup=reply_markup)
 
     # 如果记录已存在，执行 变更 picpath
 
@@ -296,13 +291,13 @@ def base_photo(update: Update, context: CallbackContext, doucment_pt: str | None
 
 
 # 解决 颜色三个状态
-def button_update(update: Update, context: CallbackContext):
+async def button_update(update: Update, context: ContextTypes.DEFAULT_TYPE):
     same_primary_key = update.effective_user.id
     existing_user: CreateThemeLogo | None = session.get(CreateThemeLogo, same_primary_key)
     query = update.callback_query
     # 检查
     if not existing_user or query.message.message_id != existing_user.callback_id:
-        query.answer("此键盘不属于你，点击无效呢！")
+        await query.answer("此键盘不属于你，点击无效呢！")
         return
 
     if existing_user.flag == 1:
@@ -312,8 +307,7 @@ def button_update(update: Update, context: CallbackContext):
         callback_desktop.callback_desktop_handle(update, context)
 
 
-
-def parse_document(update: Update, context: CallbackContext):
+async def parse_document(update: Update, context: ContextTypes.DEFAULT_TYPE):
     same_primary_key = update.effective_user.id
     existing_user: CreateThemeLogo | None = session.get(CreateThemeLogo, same_primary_key)
 
@@ -326,7 +320,7 @@ def parse_document(update: Update, context: CallbackContext):
         return
 
     if not hasattr(update.message, "document"):
-        base_photo(update, context)
+        await base_photo(update, context)
         return
 
     document = update.message.document
@@ -337,22 +331,21 @@ def parse_document(update: Update, context: CallbackContext):
         return
 
     try:
-
-        file_obj: File = document.get_file()
+        file_obj: File = await document.get_file()
         file_path: str = "src/Photo/" + str(update.effective_user.id) + ".png"
-        public_IO: IO = file_obj.download(file_path)
+        public_IO = await file_obj.download_to_drive(file_path)
         Image.open(public_IO)
 
     except Exception:
         # 如果无法打开图像，它不是一个有效的图像文件
-        update.message.reply_text(f"您发送了非法文件")
+        await update.message.reply_text(f"您发送了非法文件")
         return
-    update.message.reply_text("嗯嗯！这确实是一个图片")
+    await update.message.reply_text("嗯嗯！这确实是一个图片")
 
-    base_photo(update, context, file_path)
+    await base_photo(update, context, file_path)
 
 
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
     if args:
         path = base64.b64decode(args[0]).decode()
@@ -362,10 +355,10 @@ def start(update: Update, context: CallbackContext):
                 ppath = path.replace("tdesktop-theme", "jpg")
                 with open("src/myserver_bot_public/desk/" + ppath, "rb") as pd:  # 图片加载
                     preview_bytes = pd.read()
-                    context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
+                    await context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
                     if preview_bytes:
-                        context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
-                    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+                        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
 
         if path.endswith("attheme"):
             with open("src/myserver_bot_public/attheme/" + path, "rb") as fd:
@@ -373,53 +366,50 @@ def start(update: Update, context: CallbackContext):
                 ppath = path.replace("attheme", "jpg")
                 with open("src/myserver_bot_public/attheme/" + ppath, "rb") as pd:  # 图片加载
                     preview_bytes = pd.read()
-                    context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
+                    await context.bot.send_document(chat_id=update.effective_chat.id, document=data, filename=path)
                     if preview_bytes:
-                        context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
-                    context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
-        # update.message.reply_text(f'Command arguments: {args}')
+                        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=preview_bytes)
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text="这是您的主题文件，亲～")
+        # await update.message.reply_text(f'Command arguments: {args}')
     else:
         # 优化说明显示
-        context.bot.send_message(chat_id=update.effective_chat.id, text=strinfo)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=str_info)
         logger.info("可能为私聊 {}".format(str(update)))
 
 
 if __name__ == "__main__":
     logger.info("运行本项目必须开启ThemeFactory")
     # 同步 桌面主题
-
-
-
     sync_dp()
     sunc_ap()
     # 同步 安卓主题
-    # dispatcher.add_handler(MessageHandler(Filters.all, filter_user), group=-1)
-    dispatcher.add_handler(MessageHandler(Filters.document.category("image"), parse_document))
-    dispatcher.add_handler(MessageHandler(Filters.photo, base_photo))
+    # application.add_handler(MessageHandler(filters.ALL, filter_user), group=-1)
+    application.add_handler(MessageHandler(filters.Document.IMAGE, parse_document))
+    application.add_handler(MessageHandler(filters.PHOTO, base_photo))
     # 基于图片创建 attheme主题
-    dispatcher.add_handler(CommandHandler('create_attheme_base_pic', create_attheme))
+    application.add_handler(CommandHandler('create_attheme_base_pic', create_attheme))
 
-    dispatcher.add_handler(CallbackQueryHandler(button_update))
+    application.add_handler(CallbackQueryHandler(button_update))
     # 基于图片创建 tdesktop主题
-    dispatcher.add_handler(CommandHandler('create_tdesktop_base_pic', create_tdesktop))
+    application.add_handler(CommandHandler('create_tdesktop_base_pic', create_tdesktop))
 
     # 随机获取主题
-    dispatcher.add_handler(CommandHandler('getandroidtheme', get_android_theme))
-    dispatcher.add_handler(CommandHandler('getiostheme', get_ios_theme))
-    dispatcher.add_handler(CommandHandler('getdesktoptheme', get_desktop_theme))
-    dispatcher.add_handler(CommandHandler('getrandomtheme', get_ran_theme))
+    application.add_handler(CommandHandler('getandroidtheme', get_android_theme))
+    application.add_handler(CommandHandler('getiostheme', get_ios_theme))
+    application.add_handler(CommandHandler('getdesktoptheme', get_desktop_theme))
+    application.add_handler(CommandHandler('getrandomtheme', get_ran_theme))
 
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('report', write_ban_word))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('report', write_ban_word))
 
-    dispatcher.add_handler(CommandHandler('combinthemeandphoto', combin_theme))
-    dispatcher.add_handler(CommandHandler('getbackground', combin_theme))
+    application.add_handler(CommandHandler('combinthemeandphoto', combin_theme))
+    application.add_handler(CommandHandler('getbackground', combin_theme))
 
-    dispatcher.add_handler(MessageHandler(Filters.text, admin_handle))
-    dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, on_join))
+    application.add_handler(MessageHandler(filters.TEXT, admin_handle))
+    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, on_join))
 
     if os.environ.get('ENV') != 'dev':
-        dispatcher.add_error_handler(error_handler)
+        application.add_error_handler(error_handler)
 
-    updater.start_polling()
+    application.run_polling()
     run()
