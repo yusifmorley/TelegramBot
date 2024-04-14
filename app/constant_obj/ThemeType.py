d999b2a19bfb6c120c9ed089f6cd8c7701c02753
YusifMorley <yusifmorley@gmail.com>
2024-04-14 12:30:03 +0800

    逻辑优化

--- app/constant_obj/ThemeType.py
+++ app/constant_obj/ThemeType.py
@@ -1,5 +1,8 @@
+from app.logger import t_log
 from app.util.sync_public_attheme import get_attheme_list
 from app.util.sync_public_desk import get_desk_list
+
+logger = t_log.get_logging().getLogger(__name__)
 
 
 class ThemeType(object):
@@ -9,12 +12,14 @@ class ThemeType(object):
 
     def flushdict(self):
         self.lic = dict(get_desk_list(), **get_attheme_list())
+        logger.info("刷新type字典完成")
 
     def get_ty_list(self):
         return self.lic
 
 
 t_y = ThemeType()
+
 
 def get_theme_list():
     return t_y
