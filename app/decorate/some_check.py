50572e3fa7f8c2894021f38866093e6c25ab60fe
YusifMorley <yusifmorley@gmail.com>
2024-05-18 16:04:05 +0800

    排除命令1

--- app/decorate/some_check.py
+++ app/decorate/some_check.py
@@ -11,11 +11,15 @@ from datetime import date
 
 logger = get_logging().getLogger(__name__)
 
+
 # 用户使用监听
 # 如果被封禁了就不可使用机器人
 def some_check(fun):
     async def add_check(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        logger.info(f"{update}")
+        if update.effective_chat.id == -1001313322278:
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text="此命令已经在本群组中禁用。如若使用请私聊我~")
+
         await fun(update, context)
 
     return add_check
