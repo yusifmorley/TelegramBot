a074254ced2dc061868189d7d1fa105a2307865b
YusifMorley <yusifmorley@gmail.com>
2024-05-19 16:41:23 +0800

    删除命令

--- app/decorate/delete_command.py
+++ app/decorate/delete_command.py
@@ -8,7 +8,7 @@ def delete_command(fun):
     async def add_delete_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.effective_chat.id == -1001313322278:
             logger.info(f"{update}")
-            await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+            await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.id)
         await fun(update, context)
 
     return add_delete_command
