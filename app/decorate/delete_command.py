d191d3677790a7a40f7ee2a9c3301ca2c3a03161
YusifMorley <yusifmorley@gmail.com>
2024-05-19 16:36:58 +0800

    删除命令

--- app/decorate/delete_command.py
+++ app/decorate/delete_command.py
@@ -5,7 +5,7 @@ from telegram.ext import ContextTypes
 def delete_command(fun):
     async def add_delete_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.effective_chat.id == -1001313322278:
-            context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+            await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
         await fun(update, context)
 
     return add_delete_command
