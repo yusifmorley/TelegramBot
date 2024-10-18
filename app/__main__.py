dd4d818fb4ed4354990a4e0a4a5a3a4da64f432f
YusifMorley <yusifmorley@gmail.com>
2024-10-18 11:31:58 +0800

    seesion优化

--- app/__main__.py
+++ app/__main__.py
@@ -370,29 +370,30 @@ if __name__ == "__main__":
 
     # 同步 安卓主题
     # application.add_handler(MessageHandler(filters.ALL, filter_user), group=-1)
-    application.add_handler(MessageHandler(filters.Document.IMAGE, parse_document))
-    application.add_handler(MessageHandler(filters.PHOTO, base_photo))
+    # application.add_handler(MessageHandler(filters.Document.IMAGE, parse_document))
+    # application.add_handler(MessageHandler(filters.PHOTO, base_photo))
     # 基于图片创建 attheme主题
-    application.add_handler(CommandHandler('create_attheme_base_pic', create_attheme))
+    # application.add_handler(CommandHandler('create_attheme_base_pic', create_attheme))
 
-    application.add_handler(CallbackQueryHandler(button_update))
+    # application.add_handler(CallbackQueryHandler(button_update))
     # 基于图片创建 tdesktop主题
-    application.add_handler(CommandHandler('create_tdesktop_base_pic', create_tdesktop))
+    # application.add_handler(CommandHandler('create_tdesktop_base_pic', create_tdesktop))
 
     # 随机获取主题
-    application.add_handler(CommandHandler('getandroidtheme', get_android_theme))
+    # application.add_handler(CommandHandler('getandroidtheme', get_android_theme))
     application.add_handler(CommandHandler('getiostheme', get_ios_theme))
-    application.add_handler(CommandHandler('getdesktoptheme', get_desktop_theme))
-    application.add_handler(CommandHandler('getrandomtheme', get_ran_theme))
 
-    application.add_handler(CommandHandler('start', start))
+    # application.add_handler(CommandHandler('getdesktoptheme', get_desktop_theme))
+    # application.add_handler(CommandHandler('getrandomtheme', get_ran_theme))
+
+    # application.add_handler(CommandHandler('start', start))
     # application.add_handler(CommandHandler('report', write_ban_word))
 
-    application.add_handler(CommandHandler('combinthemeandphoto', combin_theme))
-    application.add_handler(CommandHandler('getbackground', combin_theme))
+    # application.add_handler(CommandHandler('combinthemeandphoto', combin_theme))
+    # application.add_handler(CommandHandler('getbackground', combin_theme))
 
     # application.add_handler(MessageHandler(filters.TEXT, admin_handle))
-    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, on_join))
+    # application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, on_join))
     application.add_error_handler(error_handler)
 
     # 开启辅助线程
